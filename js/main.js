6a836b44c2c2370e48e0ea2589e8560bdc485151
sagarkaryamapudi <85895514+sagarkaryamapudi@users.noreply.github.com>
2025-10-03 00:59:08 -0400

    upd

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 // Dark mode toggle
 const toggle = document.getElementById("darkModeToggle");
-toggle.addEventListener("click", () => {
-  document.body.classList.toggle("dark");
-  toggle.textContent = document.body.classList.contains("dark") ? "☀️" : "🌙";
-});
+if(toggle){
+  toggle.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+    toggle.textContent = document.body.classList.contains("dark") ? "☀️" : "🌙";
+  });
+}
