aa46374cf68aeb49c74851c87304cb58d9b484fe
sagarkaryamapudi <85895514+sagarkaryamapudi@users.noreply.github.com>
2025-10-03 01:06:13 -0400

    updated

--- js/main.js
+++
@@ -1,8 +0,0 @@
-// Dark mode toggle
-const toggle = document.getElementById("darkModeToggle");
-if(toggle){
-  toggle.addEventListener("click", () => {
-    document.body.classList.toggle("dark");
-    toggle.textContent = document.body.classList.contains("dark") ? "☀️" : "🌙";
-  });
-}
